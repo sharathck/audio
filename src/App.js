1c8e8f30ff172ab449f8e4a7ab7b2adf9226d53e
Sharath Chandra <sharathck@outlook.com>
2024-04-10 17:45:33 -0500

    Add new dependencies and update CSS styles in App.js and App.css

--- src/App.js
+++ src/App.js
@@ -1,22 +1,82 @@
-import logo from './logo.svg';
 import './App.css';
+import ReactPlayer from 'react-player';
+import React, { useEffect, useState, useRef } from 'react';
+import { PlayCircleOutlined, PauseCircleOutlined } from '@ant-design/icons';
 
 function App() {
+  const [files, setFiles] = useState([]);
+  const [currentFileUrl, setCurrentFileUrl] = useState('');
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [progress, setProgress] = useState(0);
+  const playerRef = useRef(null);
+
+  useEffect(() => {
+    fetch('http://localhost:4500/files')
+      .then(response => response.json())
+      .then(data => {
+        console.log('Data fetched: ', data);
+        setFiles(data);
+      })
+      .catch(error => console.error('Error fetching data: ', error));
+  }, []);
+
+  const setAudioSource = (url) => {
+    setCurrentFileUrl(url);
+    setIsPlaying(true);
+  };
+
+  const handlePlayPause = () => {
+    setIsPlaying(!isPlaying);
+  };
+
+  const handleProgress = (progress) => {
+    setProgress(progress.played);
+  };
+
+  const handleSeek = (e) => {
+    const seekTime = parseFloat(e.target.value);
+    playerRef.current.seekTo(seekTime);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <input
+          type="range"
+          min="0"
+          max="1"
+          step="0.01"
+          value={progress}
+          onChange={handleSeek}
+          style={{ position: 'relative',width: '90%', height: '20%' }} // Increase width and height here
+        />
+        <div className=''>
+          <ReactPlayer
+            ref={playerRef}
+            url={currentFileUrl}
+            playing={isPlaying}
+            controls={true}
+            width="800"
+            height="300"
+            onError={(error) => console.error('Error loading or playing audio:', error)}
+            onProgress={handleProgress}
+            style={{ position: 'fixed',width: '90%', height: '20%' }}
+          />
+          {isPlaying ? (
+            <PauseCircleOutlined onClick={handlePlayPause} style={{ fontSize: '68px', color: 'red' }} />
+          ) : (
+            <PlayCircleOutlined onClick={handlePlayPause} style={{ fontSize: '54px', color: 'green' }} />
+          )}
+        </div>
+        <ul>
+          {files.map((file, index) => (
+            <li key={index}>
+              <a href={file.url} onClick={(e) => { e.preventDefault(); setAudioSource(file.url); }}>
+                {file.name}
+              </a>
+            </li>
+          ))}
+        </ul>
       </header>
     </div>
   );

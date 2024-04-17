ddb51225253c1a200ee824cc8ba02283e4bff24d
Sharath <sharathck@outlook.com>
2024-04-16 21:53:56 -0500

    Refactor file list styling and dimensions in App.js and App.css

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import './App.css';
-import ReactPlayer from 'react-player';
 import React, { useEffect, useState, useRef } from 'react';
-import { PlayCircleOutlined, PauseCircleOutlined } from '@ant-design/icons';
+import H5AudioPlayer from 'react-h5-audio-player';
+import 'react-h5-audio-player/lib/styles.css';
 
 function App() {
   const [files, setFiles] = useState([]);
   const [currentFileUrl, setCurrentFileUrl] = useState('');
   const [isPlaying, setIsPlaying] = useState(false);
-  const [progress, setProgress] = useState(0);
   const [selectedFileIndex, setSelectedFileIndex] = useState(null);
   const playerRef = useRef(null);
 
@@ -27,56 +26,24 @@ function App() {
     setSelectedFileIndex(index);
   };
 
-  const handlePlayPause = () => {
-    setIsPlaying(!isPlaying);
-  };
-
-  const handleProgress = (progress) => {
-    setProgress(progress.played);
-
-    // Check if the current file has finished playing
-    if (progress.played === 1 && selectedFileIndex !== null && selectedFileIndex < files.length - 1) {
-      // Play the next file in the list
-      const nextFileIndex = selectedFileIndex + 1;
-      const nextFileUrl = files[nextFileIndex].url;
-      setAudioSource(nextFileUrl, nextFileIndex);
-    }
-  };
-
-  const handleSeek = (e) => {
-    const seekTime = parseFloat(e.target.value);
-    playerRef.current.seekTo(seekTime);
-  };
-
   return (
     <div className="App">
       <header className="App-header">
-        <input
-          type="range"
-          min="0"
-          max="1"
-          step="0.01"
-          value={progress}
-          onChange={handleSeek}
-          style={{ position: 'relative', width: '90%', height: '40px' }} // Increase width and height here
-        />
-        <div className="">
-          <ReactPlayer
+        <div className="" style={{ width: '80%' }}>
+          <H5AudioPlayer
             ref={playerRef}
-            url={currentFileUrl}
-            playing={isPlaying}
+            src={currentFileUrl}
+            autoPlay={isPlaying}
             controls={true}
-            width="800"
-            height="300"
             onError={(error) => console.error('Error loading or playing audio:', error)}
-            onProgress={handleProgress}
-            style={{ position: 'fixed', width: '90%', height: '20%' }}
+            onPlay={() => setIsPlaying(true)}
+            onPause={() => setIsPlaying(false)}
+            onEnded={() => {
+              const nextFileIndex = (selectedFileIndex + 1) % files.length;
+              setAudioSource(files[nextFileIndex].url, nextFileIndex);
+            }}
           />
-          {isPlaying ? (
-            <PauseCircleOutlined onClick={handlePlayPause} style={{ fontSize: '68px', color: 'red' }} />
-          ) : (
-            <PlayCircleOutlined onClick={handlePlayPause} style={{ fontSize: '54px', color: 'green' }} />
-          )}
+
         </div>
         <ul style={{ listStyleType: 'none' }}>
           {files.map((file, index) => (
